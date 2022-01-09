de0f241035372f04f05c14ab4600411a550c8df0
Muhammad Hidayatullah <dayatwork095@gmail.com>
2022-01-09 11:43:22 +0700

    admin: events

---
+++ pages/admin/events/[id].tsx
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import Link from "next/link";
+import dynamic from "next/dynamic";
+
+import { AdminLayout } from "@/components/layouts";
+import { FormLabel, Input, Textarea, Switch } from "@/components/forms";
+import { Button } from "@/components/elements";
+
+const RichText = dynamic(() => import("@/components/rich-text/RichText"), {
+  ssr: false,
+});
+
+/* eslint-disable-next-line */
+export interface EditEventProps {}
+
+export function EditEvent(props: EditEventProps) {
+  const [content, setContent] = useState("");
+
+  return (
+    <AdminLayout>
+      <div className="py-4">
+        <Link href="/admin/events">
+          <a className="block mb-2 text-sm text-primary-700 hover:underline">
+            &larr; Back to event list
+          </a>
+        </Link>
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl font-bold text-gray-900">Edit Event</h1>
+          <Button variant="danger">Delete</Button>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-10">
+        <form>
+          <div className="shadow sm:rounded-md sm:overflow-hidden">
+            <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+              <div>
+                <FormLabel htmlFor="title">Title</FormLabel>
+                <div className="mt-1">
+                  <Input id="title" />
+                </div>
+              </div>
+
+              <div>
+                <FormLabel htmlFor="description">Description</FormLabel>
+                <div className="mt-1">
+                  <Textarea id="description" />
+                </div>
+              </div>
+
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Image
+                </label>
+                <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                  <div className="space-y-1 text-center">
+                    <svg
+                      className="mx-auto h-12 w-12 text-gray-400"
+                      stroke="currentColor"
+                      fill="none"
+                      viewBox="0 0 48 48"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                    <div className="flex text-sm text-gray-600">
+                      <label
+                        htmlFor="file-upload"
+                        className="relative cursor-pointer bg-white rounded-md font-medium text-primary-600 hover:text-primary-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-primary-500"
+                      >
+                        <span>Upload a file</span>
+                        <input
+                          id="file-upload"
+                          name="file-upload"
+                          type="file"
+                          className="sr-only"
+                        />
+                      </label>
+                      <p className="pl-1">or drag and drop</p>
+                    </div>
+                    <p className="text-xs text-gray-500">
+                      PNG, JPG, GIF up to 10MB
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <FormLabel htmlFor="content">Content</FormLabel>
+                <div className="mt-1">
+                  <RichText value={content} onChange={setContent} />
+                </div>
+              </div>
+
+              <div>
+                <FormLabel id="status">Publish</FormLabel>
+                <div className="mt-1">
+                  <Switch id="status" />
+                </div>
+              </div>
+            </div>
+            <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+              <button
+                type="submit"
+                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </AdminLayout>
+  );
+}
+
+export default EditEvent;
